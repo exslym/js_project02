4138f7be7bcafce665bef41c9d5fb6324d568f47
exslym <76844562+exslym@users.noreply.github.com>
2021-07-21 22:52:22 +0300

    add function addZero to timer

--- src/js/script.js
+++ src/js/script.js
@@ -42,26 +42,16 @@ window.addEventListener('DOMContentLoaded', function() {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
-            hours = Math.floor((t/(1000*60*60)));
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
+            hours = Math.floor(t/1000/60/60);
+/*          hours = Math.floor((t/1000/60/60) % 24),
+            days = Math.floor(t/1000/60/60/24); */
 
-/*          hours = Math.floor(t/(1000/60/60) % 24),
-            days = Math.floor(t/(1000*60*60*24)); */
         return {
             'total' : t,
             'hours' : hours,
             'minutes' : minutes,
             'seconds' : seconds
         };
-
     }
 
     function setClock(id, endtime) {
@@ -73,12 +63,23 @@ window.addEventListener('DOMContentLoaded', function() {
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+
+            function addZero(num) {
+                if (num < 10) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
 
             if (t.total <= 0) {
                 clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
         }
     }
