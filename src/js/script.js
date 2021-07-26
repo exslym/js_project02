ce4805b86e740c0b8f3771839f4a60b8a1862fa5
exslym <76844562+exslym@users.noreply.github.com>
2021-07-26 20:30:08 +0300

    Added modal window on the other buttons

--- src/js/script.js
+++ src/js/script.js
@@ -88,7 +88,8 @@ window.addEventListener('DOMContentLoaded', function () {
 
 	let more = document.querySelector('.more'),
 		overlay = document.querySelector('.overlay'),
-		close = document.querySelector('.popup-close');
+		close = document.querySelector('.popup-close'),
+		descr = document.querySelectorAll('.description-btn');
 
 	more.addEventListener('click', function () {
 		overlay.style.display = 'block';
@@ -100,5 +101,13 @@ window.addEventListener('DOMContentLoaded', function () {
 		overlay.style.display = 'none';
 		more.classList.remove('more-splash');
 		document.body.style.overflow = '';
+	});
+
+	descr.forEach(function (item) {
+		item.addEventListener('click', function () {
+			overlay.style.display = 'block';
+			this.classList.add('more-splash');
+			document.body.style.overflow = 'hidden';
+		});
 	});
 });
